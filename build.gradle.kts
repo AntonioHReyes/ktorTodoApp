714aab52d0b246c844f12d97e8bdb28b81d89acb
Antonio Huerta Reyes <anhr9728@gmail.com>
2021-07-18 18:15:45 -0500

    Agrega configuración para despliegue en google cloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,30 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val ktorm_version: String by project
+val appengine_version: String by project
+val appengine_plugin_version: String by project
+
+buildscript {
+    repositories {
+        jcenter()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath("com.google.cloud.tools:appengine-gradle-plugin:2.2.0")
+    }
+}
+
+apply(plugin="war")
+apply(plugin="com.google.cloud.tools.appengine")
+
+configure<com.google.cloud.tools.gradle.appengine.standard.AppEngineStandardExtension> {
+    deploy {
+        projectId = "ktorapps"
+        version = "1"
+        stopPreviousVersion = true // etc
+    }
+}
 
 plugins {
     application
@@ -15,12 +39,14 @@ application {
 }
 
 repositories {
+    jcenter()
     mavenCentral()
 }
 
 dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-server-servlet:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
@@ -31,4 +57,8 @@ dependencies {
     implementation("mysql:mysql-connector-java:8.0.11")
     implementation("org.ktorm:ktorm-support-mysql:${ktorm_version}")
     implementation("org.ktorm:ktorm-core:${ktorm_version}")
+
+    //AppEngine
+    compileOnly("com.google.appengine:appengine:+")
+    compileOnly("javax.servlet:javax.servlet-api:3.1.0")
 }
